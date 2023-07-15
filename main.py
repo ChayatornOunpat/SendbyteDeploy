7f9c447ed042ec05af6db3a2a7bf0030521bc7bb
ChayatornOunpat <89130694+ChayatornOunpat@users.noreply.github.com>
2023-07-15 21:29:15 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,104 @@
+import os
+import secrets
+import asyncio
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+
+
+app = FastAPI()
+
+
+time = {}
+
+origins = [
+    "http://sendbyte.net"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+async def delete():
+    while True:
+        await asyncio.sleep(60)
+        remove = []
+        for key in time.keys():
+            time[key] = time[key] - 1
+            if time[key] == 0:
+                remove.append(key)
+                if os.path.exists(f'./files/{key}_file.zip'):
+                    os.remove(f'./files/{key}_file.zip')
+        for key in remove:
+            del time[key]
+
+
+@app.on_event("startup")
+async def start():
+    t = asyncio.create_task(delete())
+
+
+@app.get("/")
+async def root():
+    return FileResponse('./dist/index.html')
+
+@app.get("/send")
+async def send():
+    return FileResponse('./dist/index.html')
+
+@app.get("/receive")
+async def receive():
+    return FileResponse('./dist/index.html')
+
+@app.get("/receive/{code}")
+async def receive(code: int):
+    return FileResponse('./dist/index.html')
+
+@app.get("/js")
+async def js():
+    return FileResponse('./dist/assets/index-15e7c39d.js')
+
+@app.get("/css")
+async def css():
+    return FileResponse('./dist/assets/index-c1357765.css')
+
+@app.get("/bg/home")
+async def home_bg():
+    return FileResponse('./assets/home.jpg')
+
+@app.get("/bg/send")
+async def send_bg():
+    return FileResponse('./assets/send.jpg')
+
+@app.get("/bg/receive")
+async def receive_bg():
+    return FileResponse('./assets/receive.jpg')
+
+@app.get("/icon")
+async def icon():
+    return FileResponse('./assets/icon.png')
+
+@app.get("/receive/files/{fn}")
+async def download(fn: int):
+    if os.path.exists(f"./files/{fn}_file.zip"):
+        return FileResponse(f"./files/{fn}_file.zip")
+    else:
+        raise HTTPException(status_code=404, detail="File not found")
+
+@app.post("/send/submitfile")
+async def upload(file: UploadFile = File(...)):
+    while True:
+        name = secrets.randbelow(999999)
+        if name not in time.keys():
+            filename = name
+            with open(f'./files/{filename}_file.zip', 'wb') as f:
+                contents = await file.read()
+                f.write(contents)
+            time[name] = 10
+
+            return {"code": filename}
